7a2ad9e4c9be3f6cdb27b95cefeba5bf5c821788
Ron Knight <ron.knight@gmail.com>
2020-01-15 09:14:48 -0600

    Add files via upload
    Keeps Chrome from updating itself. Useful if you can't use a GP.

---
+++ ChromeAutoUpdateOFF.ps1
@@ -0,0 +1,73 @@
+﻿<#	
+	.NOTES
+#===========================================================================
+	Created by:   	Ron Knight
+	Date:          01.08.2020
+	Organization: 	Builders First Source
+	Version: 
+	Filename:    ChromeAutoUpdateOFF.ps1
+#===========================================================================
+	.DESCRIPTION
+		This installs Google Chrome v. 79.0.3945.117
+	
+	Revision Notes:
+#>
+
+#===========================================================================
+#Set Variables for script
+$AppName = "ChromeAutoUpdateOFF" #Name of application as it will appear in the log
+$LogPath = "C:\temp\BFSlogs" #location of log file
+$LogDate = get-date -format "MM-dd-yyyy"
+$OS = Get-WmiObject Win32_OperatingSystem
+$OSCap = $OS.Caption
+$Arch = $OS.OSArchitecture
+$OSBuild = $OS.BuildNumber
+$ComputerSystem = Get-WmiObject Win32_ComputerSystem
+if ($ComputerSystem.Manufacturer -like 'Lenovo') { $Model = (Get-WmiObject Win32_ComputerSystemProduct).Version }
+            else { $Model = $ComputerSystem.Model }
+$LastLogon = (Get-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Authentication\LogonUI\').LastLoggedOnUser
+
+
+#===========================================================================
+#Begin Logging Script
+if (!(test-path C:\temp\BFSlogs))
+{
+    Write-Output "Log folder for BFS does not exist - creating it."
+	New-Item -ItemType Directory -Path C:\temp\BFSlogs -Verbose
+	Start-Transcript -Path "$LogPath\$AppName.$LogDate.log" -Force -Append
+}
+
+else { 
+	Start-Transcript -Path "$LogPath\$AppName.$LogDate.log" -Force -Append
+}
+
+#===========================================================================
+Write-Output "### Script Start ###"
+Write-Output "Start time: $(Get-Date)"
+Write-Output "Username: $(([Environment]::UserDomainName + "\" + [Environment]::UserName))"
+Write-Output "Last Logged On User: $lastlogon"
+Write-Output "Computer Name: $(& hostname)"
+Write-Output "Operating System: $OSCap $Arch"
+Write-Output "Build = $OSBuild"
+Write-Output "Computer Model: $Model"
+#===========================================================================
+
+Write-Output "Modifying Registry to disable AutoUpdate"
+
+New-Item -Path "HKLM:\SOFTWARE\Policies\Google" -Name \Update –Force
+
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Google\Update" -Name “AutoUpdateCheckPeriodMinutes” -Value 0
+Write-Output "AutoUpdateCheckPeriodMinutes value set to 0"
+
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Google\Update" -Name “DisableAutoUpdateChecksCheckboxValue” -Value 0
+Write-Output "DisableAutoUpdateChecksCheckboxValue value set to 0"
+
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Google\Update" -Name “UpdateDefault” -Value 0
+Write-Output "UpdateDefault value set to 0"
+
+Write-Output "All registry entries created."
+#===========================================================================
+
+Write-Output "$AppName script installation Complete."
+# ===========================================================================
+Stop-Transcript
